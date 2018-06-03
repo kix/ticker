166bf9110e98dc00a1644a9dc3fbc42634de9a88
Stepan Anchugov <kixxx1@gmail.com>
2018-06-03 19:54:30 +0500

    Add a Cucumber test

--- features/price_display.feature
+++ features/price_display.feature
@@ -1,5 +1,14 @@
 Feature: Price display
+    Scenario: Price can be fixed
+        When I open admin page
+        And I set a fixed price of 1000 for 5 seconds
+        And I open root page
+        Then I see a price of 1000
+        When I wait for 5 seconds
+        And the latest price is 7500
+        And I open root page
+        Then I see a price of 7500
     Scenario: Latest known price is displayed
-        Given a price has updated
+        Given the latest price is 7500
         When I open root page
-        Then I see the latest price
+        Then I see a price of 7500
--- features/step_definitions/stepdefs.rb
+++ features/step_definitions/stepdefs.rb
@@ -1,13 +1,45 @@
 require 'rubygems'
 
-Given("a price has updated") do
-  Rails.cache.write 'rate', 1000
+Capybara.register_driver :selenium do |app|
+  Capybara::Selenium::Driver.new(app, :browser => :chrome)
 end
 
+Capybara.configure do |config|
+    config.default_max_wait_time = 10 # seconds
+    config.default_driver        = :selenium
+end
+
+Given("the latest price is {int}") do |int|
+    Rails.cache.write 'rate', int
+end
+  
+When("I set a fixed price of {int} for {int} seconds") do |price, duration|
+    in_5_secs = Time.now + duration
+    fill_in('Rate', with: price)
+    fill_in('Until', with: in_5_secs.strftime('%d/%m/%Y %H:%m'))
+    find('[type=submit]').click
+end
+  
+Then("I see a price of {int}") do |price|
+    page.has_content? price
+end
+  
+When("I wait for {int} seconds") do |seconds|
+    sleep seconds
+end
+  
 When("I open root page") do
-    visit root_path
+    visit '/'
 end
 
-Then("I see the latest price") do
-    expect(page).to have_content "$1000"
+Then("I see a price of (\d+)") do |price|
+    page.has_content? "$1000"
+end
+
+When("I open admin page") do
+    visit '/admin'
+end
+
+When("I set a fixed price of (\d+) for (\d+) seconds") do |price, duration|
+    
 end
